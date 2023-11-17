4cc9020a64b9bdcf14430f20102a2e3f5bb3bc84
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-17 22:40:45 +0700

    Fix Header Style

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,7 +1,7 @@
 const NoteHeader = () => {
   return (
-    <header className=" border-sub grid w-full border-b-2 p-5 text-center font-bold uppercase">
-      <p className=" m-auto w-full text-left">Personal Notes</p>
+    <header className=" grid w-full border-b-2 border-sub p-5 text-center font-bold uppercase">
+      <p className=" m-auto w-full text-left text-accent">Personal Notes</p>
     </header>
   );
 };
