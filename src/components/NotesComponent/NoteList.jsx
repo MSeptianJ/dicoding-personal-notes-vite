29105783cffea7b794210e179e546d3c4b4c2999
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-21 09:21:56 +0700

    Add Element if there are no notes

--- src/components/NotesComponent/NoteList.jsx
+++ src/components/NotesComponent/NoteList.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
-import NoteItem from "./NoteItem";
+import NoteCard from "./NoteCard";
 
 const NoteList = ({ ListTitle, ListNotes }) => {
   return (
     <div className=" w-full rounded-sm bg-sub p-3">
-      <h2 className=" mb-5 font-semibold uppercase">{ListTitle}</h2>
-      <div className=" m-auto grid w-full grid-cols-4 gap-5">
-        {ListNotes?.map((note, id) => (
-          <NoteItem key={id} noteData={note} />
-        ))}
+      <h2 className=" mb-2 font-semibold uppercase text-accent">{ListTitle}</h2>
+      <div className=" m-auto grid w-full gap-5 md:grid-cols-2 lg:grid-cols-4">
+        {ListNotes.length <= 0 ? (
+          <p className=" text-center">No Notes</p>
+        ) : (
+          ListNotes?.map((note, id) => <NoteCard key={id} noteData={note} />)
+        )}
       </div>
     </div>
   );
