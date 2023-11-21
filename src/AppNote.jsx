2d57f3e99757993525ee010676387c4e978c3c86
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-21 15:20:33 +0700

    Add Search Note Feature

--- src/AppNote.jsx
+++ src/AppNote.jsx
@@ -7,8 +7,16 @@ import { getInitialData } from "./utils/utils";
 
 function AppNote() {
   const [notes, setNotes] = useState(getInitialData);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const data = [];
+  const activeNotes = notes.filter(
+    (note) =>
+      note.archived === false && note.title.toLowerCase().includes(searchQuery),
+  );
+  const archiveNotes = notes.filter(
+    (note) =>
+      note.archived === true && note.title.toLowerCase().includes(searchQuery),
+  );
 
   const handlerAddNote = (newData) => {
     const noteData = {
@@ -27,12 +35,32 @@ function AppNote() {
     setNotes(selectedNote);
   };
 
+  const handlerSearchNote = (query) => {
+    setSearchQuery(query.toLowerCase().trim());
+  };
+
+  const handlerArchiveNote = (id) => {
+    const selectedNote = notes.map((note) =>
+      note.id === id ? { ...note, archived: true } : note,
+    );
+
+    setNotes(selectedNote);
+  };
+
+  const handlerActivateNote = (id) => {
+    const selectedNote = notes.map((note) =>
+      note.id === id ? { ...note, archived: false } : note,
+    );
+
+    setNotes(selectedNote);
+  };
+
   return (
     <div className=" min-h-screen w-full bg-main">
       <NoteHeader />
       <div className=" bg-subA w-ful m-auto grid gap-4 p-4 lg:grid-cols-4">
         <div className="flex w-full flex-col items-center gap-5">
-          <NoteSearch />
+          <NoteSearch searchNote={handlerSearchNote} />
 
           <NoteSubmit addNote={handlerAddNote} />
         </div>
@@ -40,14 +68,18 @@ function AppNote() {
         <div className=" grid w-full gap-5 lg:col-span-3">
           <NoteList
             ListTitle="Notes"
-            ListNotes={notes}
+            ListNotes={activeNotes}
             deleteNote={handlerDeleteNote}
+            archiveNote={handlerArchiveNote}
+            activateNotes={handlerActivateNote}
           />
 
           <NoteList
             ListTitle="Archive"
-            ListNotes={data}
+            ListNotes={archiveNotes}
             deleteNote={handlerDeleteNote}
+            archiveNote={handlerArchiveNote}
+            activateNote={handlerActivateNote}
           />
         </div>
       </div>
--- src/components/NoteSearch.jsx
+++ src/components/NoteSearch.jsx
@@ -1,27 +1,30 @@
 import PropTypes from "prop-types";
 
-const NoteSearch = () => {
+const NoteSearch = ({ searchNote }) => {
+  const searchHandler = (e) => {
+    searchNote(e.target.value);
+  };
+
   return (
     <div className="grid w-full gap-2 rounded-sm bg-sub p-3">
       <label className=" font-semibold uppercase" htmlFor="NoteSearch">
         Search Notes
       </label>
       <div className=" m-auto w-full">
-        <form className=" m-auto w-full" action="">
-          <input
-            className=" bg-transparent m-auto w-full rounded-md border-2 border-accent p-2 text-sm"
-            type="text"
-            name="note-search"
-            id="NoteSearch"
-          />
-        </form>
+        <input
+          className=" m-auto w-full rounded-md border-2 border-accent bg-transparent p-2 text-sm"
+          type="text"
+          name="note-search"
+          id="NoteSearch"
+          onChange={searchHandler}
+        />
       </div>
     </div>
   );
 };
 
 NoteSearch.propTypes = {
-  func: PropTypes.func,
+  searchNote: PropTypes.func,
 };
 
 export default NoteSearch;
