47c1ec29b3a228e2c40f06924d11be7b0c222f82
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-21 09:22:14 +0700

    Add NoteSubmit Form

--- src/components/NoteSubmit.jsx
+++ src/components/NoteSubmit.jsx
@@ -2,8 +2,31 @@ import PropTypes from "prop-types";
 
 const NoteSubmit = () => {
   return (
-    <div className=" bg-sub w-full rounded-sm p-3">
+    <div className=" w-full rounded-sm bg-sub p-3">
       <h2 className=" font-semibold uppercase">Submit Note</h2>
+      <form
+        action=""
+        className=" m-auto grid w-full grid-rows-6 items-center gap-1"
+      >
+        <p className=" text-sm">Title Character : 20</p>
+        <input
+          type="text"
+          className=" block w-full rounded-sm border-2 border-accent bg-transparent p-2 text-sm"
+          placeholder="Title Note"
+        />
+
+        <textarea
+          name=""
+          className=" row-span-3 block h-full w-full rounded-sm border-2 border-accent bg-transparent p-2 text-sm"
+          placeholder="Body note"
+        ></textarea>
+
+        <input
+          type="submit"
+          value="Add note"
+          className=" m-auto block w-1/2 cursor-pointer rounded-sm bg-main p-1 text-accent hover:border hover:border-main hover:bg-sub"
+        />
+      </form>
     </div>
   );
 };
