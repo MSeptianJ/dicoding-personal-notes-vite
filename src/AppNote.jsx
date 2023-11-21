f61b8655e7fd8f2bceb6b587a7a9abeb29a3449f
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-21 09:22:30 +0700

    Fix AppNote Style

--- src/AppNote.jsx
+++ src/AppNote.jsx
@@ -7,21 +7,21 @@ import { getInitialData } from "./utils/utils";
 
 function AppNote() {
   const [first] = useState(getInitialData);
-
+  const data = [];
   return (
     <div className=" min-h-screen w-full bg-main">
       <NoteHeader />
-      <div className=" bg-subA w-ful m-auto grid grid-cols-4 gap-4 p-4">
-        <div className=" col-span-3 grid w-full gap-5">
-          <NoteList ListTitle="Notes" ListNotes={first} />
-
-          <NoteList ListTitle="Archive" ListNotes={first} />
-        </div>
-
+      <div className=" bg-subA w-ful m-auto grid gap-4 p-4 lg:grid-cols-4">
         <div className="flex w-full flex-col items-center gap-5">
           <NoteSearch />
 
           <NoteSubmit />
+        </div>
+
+        <div className=" grid w-full gap-5 lg:col-span-3">
+          <NoteList ListTitle="Notes" ListNotes={first} />
+
+          <NoteList ListTitle="Archive" ListNotes={data} />
         </div>
       </div>
     </div>
