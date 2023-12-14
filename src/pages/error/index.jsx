088a3551214909c141483ae9bb4e47751eac21ce
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-14 20:48:48 +0700

    Fix Error Page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -7,8 +7,14 @@ const ErrorPage = () => {
     <div className=" grid min-h-screen w-full items-center text-center">
       <div className=" m-auto grid w-full gap-5">
         <div>
-          <h3 className=" text-lg font-bold text-red-600">Error</h3>
-          <p>{errorData.message}</p>
+          <h3 className=" text-lg font-bold text-red-600">
+            {errorData.status || "Error"}
+          </h3>
+          {errorData.status === 404 ? (
+            <p>Page Not Found</p>
+          ) : (
+            <p>{errorData.message}</p>
+          )}
         </div>
 
         <div className=" m-auto w-1/3 bg-gray-500 p-2 text-white">
