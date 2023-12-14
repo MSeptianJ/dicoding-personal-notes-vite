a086761c862ceedf57fbfae91834b89b02953106
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-14 20:31:27 +0700

    Add Error Page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -4,12 +4,16 @@ const ErrorPage = () => {
   const errorData = useRouteError();
 
   return (
-    <div>
-      <div>
-        <p>{errorData}</p>
-      </div>
-      <div>
-        <Link to={"/"}>Menu</Link>
+    <div className=" grid min-h-screen w-full items-center text-center">
+      <div className=" m-auto grid w-full gap-5">
+        <div>
+          <h3 className=" text-lg font-bold text-red-600">Error</h3>
+          <p>{errorData.message}</p>
+        </div>
+
+        <div className=" m-auto w-1/3 bg-gray-500 p-2 text-white">
+          <Link to={"/"}>Menu</Link>
+        </div>
       </div>
     </div>
   );
