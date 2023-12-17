ed8f87c92f99e3d1832c7abe85c37745680567d9
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-17 13:36:43 +0700

    Add Navigation

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+import NoteNavigation from "./NoteNavigation";
+
 const NoteHeader = () => {
   return (
-    <header className=" border-primary grid w-full border-b-2 p-5 text-center font-bold uppercase">
-      <p className=" m-auto w-full text-left">Personal Notes</p>
+    <header className=" flex w-full items-center justify-between border-b-2 border-primary text-center font-bold uppercase">
+      <p className=" m-auto w-full text-left">
+        <Link to={"/"} className=" p-3 hover:text-accent">
+          Personal Notes
+        </Link>
+      </p>
+      <NoteNavigation />
     </header>
   );
 };
---
+++ src/components/NoteNavigation.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const NoteNavigation = () => {
+  return (
+    <nav className=" w-1/2">
+      <ul className=" flex">
+        <li className=" w-full cursor-pointer bg-primary text-back hover:bg-accent">
+          <Link to={"/"} className=" block w-full p-3">
+            Home
+          </Link>
+        </li>
+        <li className=" w-full cursor-pointer bg-primary text-back hover:bg-accent">
+          <Link to={"/archive"} className=" block w-full p-3">
+            Archive
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+NoteNavigation.propTypes = {};
+
+export default NoteNavigation;
