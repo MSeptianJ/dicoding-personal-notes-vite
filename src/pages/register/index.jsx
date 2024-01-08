27b92d5a032eea9ace31b7f9da7924b3ead0fa79
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-01-08 15:18:47 +0700

    Add RegisterPage

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,32 @@
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../../utils/network-data";
+import RegisterInput from "./RegisterInput";
+
 const RegisterPage = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  const registerHandler = async (name, email, password) => {
+    const { error } = await register({ name, email, password });
+
+    if (!error) {
+      navigate("/");
+    }
+  };
+
+  return (
+    <section className="m-auto w-full max-w-screen-sm rounded-[4px] bg-primary p-6 text-back shadow-lg">
+      <h2 className=" mb-5 text-xl">Fill the form to register an account</h2>
+
+      <RegisterInput registerHandler={registerHandler} />
+
+      <p className="mt-5 text-sm">
+        Already have an acount?{" "}
+        <Link to="/login" className=" underline">
+          Login Here.
+        </Link>
+      </p>
+    </section>
+  );
 };
 
 RegisterPage.propTypes = {};
