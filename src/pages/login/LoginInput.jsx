3d23d802dccf7820fe1915d48e68b1cadc9431b3
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-01-08 14:04:41 +0700

    Add LoginPage

---
+++ src/pages/login/LoginInput.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import useInput from "../../hooks/useInput";
+
+const LoginInput = ({ loginHandler }) => {
+  const [email, onEmailChange] = useInput("");
+  const [password, onPasswordChange] = useInput("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    loginHandler(email, password);
+  };
+
+  return (
+    <form className="m-auto grid w-full text-left" onSubmit={handleSubmit}>
+      <label htmlFor="loginEmail" className="mb-1">
+        Email
+      </label>
+      <input
+        type="email"
+        name="loginEmail"
+        id="loginEmail"
+        className=" mb-3 rounded-[4px] border-2 border-back bg-transparent p-2"
+        value={email}
+        onChange={onEmailChange}
+      />
+      <label htmlFor="loginPassword" className="mb-1">
+        Password
+      </label>
+      <input
+        type="password"
+        name="loginPassword"
+        id="loginPassword"
+        className=" mb-2 rounded-[4px] border-2 border-back bg-transparent p-2"
+        value={password}
+        onChange={onPasswordChange}
+      />
+      <input
+        type="submit"
+        value="Log In"
+        className="m-auto w-full cursor-pointer rounded-[4px] bg-accent px-6 py-2 font-bold transition-all hover:bg-opacity-80"
+      />
+    </form>
+  );
+};
+
+LoginInput.propTypes = {
+  loginHandler: PropTypes.func.isRequired,
+};
+
+export default LoginInput;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,32 @@
+import { Link, useOutletContext } from "react-router-dom";
+import { login } from "../../utils/network-data";
+import LoginInput from "./loginInput";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const { onloginSuccess } = useOutletContext();
+
+  const loginHandler = async (email, password) => {
+    const { error, data } = await login({ email, password });
+
+    if (!error) {
+      onloginSuccess(data);
+    }
+  };
+
+  return (
+    <section className="m-auto w-full max-w-screen-sm rounded-[4px] bg-primary p-6 text-back shadow-lg">
+      <h2 className=" mb-5 text-xl">Login to use app, please.</h2>
+
+      <LoginInput loginHandler={loginHandler} />
+
+      <p className="mt-5 text-sm">
+        Don{"'"}t have an acount?{" "}
+        <Link to="/register" className=" underline">
+          Register Here.
+        </Link>
+      </p>
+    </section>
+  );
 };
 
 LoginPage.propTypes = {};
