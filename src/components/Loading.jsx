f509ebc122d023432f3c4ccd2e502fd3f3b036f4
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2024-01-11 07:50:56 +0700

    Add Loading Component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import LOADICON from "../assets/img/loading.svg";
 const Loading = () => {
   return (
     <div className="m-auto w-full">
-      <img src={LOADICON} alt="Loading..." className="h-full w-full" />;
+      <img src={LOADICON} alt="Loading..." className="h-full w-full" />
     </div>
   );
 };
